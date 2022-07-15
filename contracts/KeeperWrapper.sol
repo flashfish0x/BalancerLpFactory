cb2ab6148a5de08c8f5701501a41b054b2e1ba05
garywackett5 <garywackett5@protonmail.com>
2022-07-15 17:11:21 +0100

    Remove previous Feat

--- contracts/KeeperWrapper.sol
+++ contracts/KeeperWrapper.sol
@@ -3,21 +3,10 @@ pragma solidity 0.6.12;
 
 interface IStrategy {
     function harvest() external;
-    function deployCredit() external;
 }
 
-contract KeeperWrapper {
-    // So indexers can keep track of this
-    event PublicHarvest(address indexed sender, address indexed _strategy);
-    event PublicInvest(address indexed sender, address indexed _strategy);
-    
+contract KeeperWrapper {   
     function harvestStrategy(address _strategy) external {
         IStrategy(_strategy).harvest();
-        emit PublicHarvest(msg.sender, _strategy);
-    }
-
-    function invest(address _strategy) external {
-        IStrategy(_strategy).deployCredit();
-        emit PublicInvest(msg.sender, _strategy);
     }
 }
