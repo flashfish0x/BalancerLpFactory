80dab64c651e3c8a97662261fe3a9f20b683e8a7
garywackett5 <garywackett5@protonmail.com>
2022-07-15 17:00:12 +0100

    Feat: Added deployCredit

--- contracts/KeeperWrapper.sol
+++ contracts/KeeperWrapper.sol
@@ -3,10 +3,21 @@ pragma solidity 0.6.12;
 
 interface IStrategy {
     function harvest() external;
+    function deployCredit() external;
 }
 
 contract KeeperWrapper {
+    // So indexers can keep track of this
+    event PublicHarvest(address indexed sender, address indexed _strategy);
+    event PublicInvest(address indexed sender, address indexed _strategy);
+    
     function harvestStrategy(address _strategy) external {
         IStrategy(_strategy).harvest();
+        emit PublicHarvest(msg.sender, _strategy);
+    }
+
+    function invest(address _strategy) external {
+        IStrategy(_strategy).deployCredit();
+        emit PublicInvest(msg.sender, _strategy);
     }
 }
